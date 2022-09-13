3e7aebed2ea2190d268cc23f2b7ab06c502e41c5
Navidj1360 <95021931+Navidj1360@users.noreply.github.com>
2022-09-13 17:31:39 +0430

    one event listener accordian

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-// getting all of acordian-content divs
-const tabs = [...document.querySelectorAll(".tab")];
-tabs.forEach((tab) => {
-  tab.addEventListener("click", (e) =>
-    e.target.nextElementSibling.classList.toggle("visible")
-  );
+const section = document.querySelector("section");
+let previosTab = null;
+section.addEventListener("click", function (e) {
+  const cellText = document.getSelection();
+  if (cellText.type === "Range") return;
+  const article = e.target.closest("article");
+  const tabIcon = article.querySelector(".tab>.accordion-icon");
+  const content = article.querySelector(".acordian-content");
+
+  if (previosTab) {
+    previosTab.querySelector(".tab>.accordion-icon").classList.remove("rotate");
+    previosTab.querySelector(".acordian-content").classList.remove("visible");
+  } 
+   previosTab = article;
+   
+  tabIcon.classList.toggle("rotate");
+  content.classList.toggle("visible");
 });
